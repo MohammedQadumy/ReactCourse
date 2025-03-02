7533322ebd24c3971f745560608b9959e820b90d
mohammedQadumy <mohammadqadumy@gmail.com>
2025-03-02 21:00:59 +0200

    deriving state from props-1

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -5,10 +5,20 @@ import {useState} from 'react'
 
 function App() {
   const [gameTurns , setGameTurns] = useState([]);
-  //const [activePlayer,setActivePlayer] = useState('X');
+  const [activePlayer,setActivePlayer] = useState('X');
 
-  function handleSelectSquare(){
+  function handleSelectSquare(row , colIndex){
     setActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : 'X' );
+    setGameTurns(prevTurns => {
+
+      let currentPlayer = 'X';
+      if(prevTurns.length > 0 && prevTurns[0].player ==='X'){
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [{ square:{row:rowIndex , col: colIndex} , player:currentPlayer} ,...prevTurns];
+      return updatedTurns;
+    });
   }
 
   return (
@@ -18,7 +28,7 @@ function App() {
             <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
             <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
     </main>
   )
