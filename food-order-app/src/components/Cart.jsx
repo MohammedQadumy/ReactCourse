72420585604b121f74a6c38d83f252b4ad598794
mohammedQadumy <mohammadqadumy@gmail.com>
2025-05-02 20:39:21 +0300

    post-failed

---
+++ food-order-app/src/components/Cart.jsx
@@ -0,0 +1,54 @@
+import { useContext } from "react";
+import CartContext from "../store/CartContext";
+import Modal from "./UI/Modal";
+import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem.jsx";
+
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
+
+  function handleGotoCheckout() {
+    userProgressCtx.showCheckout();
+  }
+
+  return (
+    <Modal
+      className="cart"
+      open={userProgressCtx.progress === "cart"}
+      onClose={userProgressCtx.progress === "" ? handleCloseCart : null}
+    >
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            item={item}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        {cartCtx.items.length > 0 && (
+          <Button onClick={handleGotoCheckout}>Go to Checkout</Button>
+        )}
+      </p>
+    </Modal>
+  );
+}
---
+++ food-order-app/src/components/Checkout.jsx
@@ -0,0 +1,81 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import Input from "./UI/Input";
+import UserProgressContext from "../store/UserProgressContext";
+import Button from "./UI/Button";
+import useHttp from "./hooks/useHttp";
+
+const requestConfig = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+export default function Checkout() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+  const {
+    data,
+    isLoading: isSending,
+    error,
+    sendRequest,
+  } = useHttp("http://localhost3000/orders", requestConfig);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  function handleClose() {
+    userProgressCtx.hideCheckout();
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    sendRequest(
+      JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      })
+    );
+  }
+
+  let actions = (
+    <>
+      <Button type="button" onClick={handleClose} textonly>
+        Close
+      </Button>
+      <Button textonly>Submit Order</Button>
+    </>
+  );
+  if(isSending){
+    actions = <span>Sending order data...</span>
+  }
+
+  return (
+    <Modal open={userProgressCtx.progress === "checkout"}>
+      <form onSubmit={handleSubmit}>
+        <h2>Checkout</h2>
+        <p>Total Amount: {cartTotal} </p>
+        <Input label="Full Name" type="text" id="name" />
+        <Input label="E-Mail Address" type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
+        <p className="modal-actions">
+            {actions}
+        </p>
+      </form>
+    </Modal>
+  );
+}
